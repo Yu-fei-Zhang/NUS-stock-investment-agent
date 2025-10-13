ad53e0eba329b280157d3c07b9d6d69bcac645b9
zhang-yufei0621 <3402906317@qq.com>
2025-10-13 15:57:51 +0800

    prompt and agent

--- stock_agent/agent/agent.py
+++ stock_agent/agent/agent.py
@@ -2,15 +2,14 @@ from __future__ import annotations
 
 from langchain.agents import ConversationalChatAgent, AgentExecutor
 from langchain.memory import ConversationBufferMemory
-from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 from langchain_openai import ChatOpenAI
 
 from stock_agent.agent.orchestration.OrchestratorPrompt import OrchestrationPrompt
 
 llm = ChatOpenAI(
-    model="gpt-4o-mini",
-    temperature=0
+    temperature=0.9,
+    api_key="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"
 )
 memory = ConversationBufferMemory(
     memory_key="chat_history",
@@ -20,4 +19,5 @@ tools = []
 agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools, system_message=OrchestrationPrompt.ROLE_PROMPT + OrchestrationPrompt.STAGE1_PROMPT
                                                    + OrchestrationPrompt.STAGE2_PROMPT + OrchestrationPrompt.STAGE3_PROMPT + OrchestrationPrompt.STAGE4_PROMPT)
 agent_executor = AgentExecutor(agent=agent,memory=memory ,tools=tools, verbose=True)
+agent_executor.invoke({"input": "你好，我想向你获取股票投资的建议"})
 
