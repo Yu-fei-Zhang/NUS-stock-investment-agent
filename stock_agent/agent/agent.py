925aff1cb08b2537a48676b5dacc2ec2b0c277e0
zhang-yufei0621 <3402906317@qq.com>
2025-10-13 14:11:31 +0800

    prompt and agent

--- stock_agent/agent/agent.py
+++ stock_agent/agent/agent.py
@@ -17,12 +17,6 @@ memory = ConversationBufferMemory(
     return_messages=True
 )
 tools = []
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "你是一个有用的助手，可以回答问题并使用工具。"),
-    ("placeholder", "{chat_history}"),
-    ("human", "{input}"),
-    ("placeholder", "{agent_scratchpad}")
-])
 agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools, system_message=OrchestrationPrompt.ROLE_PROMPT + OrchestrationPrompt.STAGE1_PROMPT
                                                    + OrchestrationPrompt.STAGE2_PROMPT + OrchestrationPrompt.STAGE3_PROMPT + OrchestrationPrompt.STAGE4_PROMPT)
 agent_executor = AgentExecutor(agent=agent,memory=memory ,tools=tools, verbose=True)
