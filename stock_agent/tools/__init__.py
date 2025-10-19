a3e08c38336297c2ff33f0ba9c7a3b29a50ccd31
ShiJunren <121040066@link.cuhk.edu.cn>
2025-10-19 14:08:08 +0800

    Changes

--- stock_agent/tools/__init__.py
+++ stock_agent/tools/__init__.py
@@ -1,3 +1,6 @@
+from stock_agent.tools.news_tools import get_company_news, get_market_news
+from stock_agent.tools.stock_data_tools import get_daily_ohlcv, get_technical_indicator
+from stock_agent.tools.stock_picker import search_stocks
 from stock_agent.tools.unified_market_data import (
     get_stock_market_data_united,
     UnifiedMarketDataTool,
@@ -24,6 +27,11 @@ __all__ = [
     "list_a_share_industry_keywords",
     "get_a_share_list_by_exact_industry",
     "IndustryCatalogTool",
+    "search_stocks",
+    "get_daily_ohlcv",
+    "get_technical_indicator",
+    "get_company_news",
+    "get_market_news"
 ]
 
 
